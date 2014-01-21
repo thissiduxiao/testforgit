69bfaced126fd368da1f01badbf6bfa0b6095289
forduxiao <915311043@qq.com>
2014-01-21 21:02:14 +0800

    test1
    test1

--- src/com/example/testgit/MainActivity.java
+++ src/com/example/testgit/MainActivity.java
@@ -10,6 +10,9 @@ public class MainActivity extends Activity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		/**
+		 * this is the test for git is github change?
+		 */
 	}
 
 	@Override
